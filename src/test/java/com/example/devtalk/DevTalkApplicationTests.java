3679db4c32f89f8028a5213092cbc44ab90a6e6d
hcgo97 <hcgo_o@naver.com>
2023-09-03 14:08:46 +0900

    Init: project init

---
+++ src/test/java/com/example/devtalk/DevTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
