92ad4bba0e4196bfd45e705df6e813bb5815e39f
minoolian <mw.lee@kakao.com>
2023-09-06 23:33:01 +0900

    feat: ChatRoom repository 추가
    * ChatRoom의 JPA 사용을 위한 Repository

---
+++ src/main/java/com/example/dm/chat/repository/ChatRoomRepository.java
@@ -0,0 +1,9 @@
+package com.example.dm.chat.repository;
+
+import com.example.dm.chat.entity.ChatRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
