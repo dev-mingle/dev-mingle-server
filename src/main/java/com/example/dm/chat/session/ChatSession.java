f8d3919844e05a875e4c0368eff23f7e93705d3f
minoolian <mw.lee@kakao.com>
2023-09-09 05:30:23 +0900

    refact: 메시지 전송 로직 이동
    - ChatSocketService 삭제
    - SendMessage 로직을 ChatSession 클래스로 이동

--- src/main/java/com/example/dm/chat/session/ChatSession.java
+++ src/main/java/com/example/dm/chat/session/ChatSession.java
@@ -1,35 +1,47 @@
 package com.example.dm.chat.session;
 
-import com.example.dm.chat.dto.ChatDto;
-import com.example.dm.chat.service.ChatRoomService;
-import com.example.dm.chat.service.ChatSocketService;
+import com.example.dm.chat.dto.ChatRoomDto;
+import com.example.dm.exception.BadRequestException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
 public class ChatSession {
 
+    private ObjectMapper mapper = new ObjectMapper();
     private Set<WebSocketSession> sessions = new HashSet<>();
 
-    public void handleSession(WebSocketSession session, ChatDto message, ChatSocketService chatSocketService) {
+    public void handleSession(WebSocketSession session, ChatRoomDto message) throws IOException{
 
         // todo: 입장문 Message DB에 저장?
-        if (message.getType().equals(ChatDto.MessageType.ENTER)) {
+        if (message.getType().equals(ChatRoomDto.MessageType.ENTER)) {
             sessions.add(session);
 
             message.setMessage(message.getSender() + "님이 입장하셨습니다");
         }
 
-        if (message.getType().equals(ChatDto.MessageType.SEND)) {
+        if (message.getType().equals(ChatRoomDto.MessageType.SEND)) {
             message.setMessage(message.getMessage());
         }
 
-        sendMessage(message, chatSocketService);
+        sendMessage(message);
 
     }
 
-    public <T> void sendMessage(T message, ChatSocketService chatSocketService) {
-        sessions.parallelStream().forEach(session -> chatSocketService.sendMessage(session, message));
+    public <T> void sendMessage(T message) throws IOException {
+        TextMessage textMessage = new TextMessage(mapper.writeValueAsString(message));
+        sessions.parallelStream().forEach(session -> {
+            try {
+                session.sendMessage(textMessage);
+            } catch (IOException e) {
+                throw new BadRequestException(e.getMessage());
+            }
+        });
     }
 }
