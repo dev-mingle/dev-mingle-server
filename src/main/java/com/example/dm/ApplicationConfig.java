8213820a7393e16f601336b964fc81b91a635b67
gwanuk <guk1502012@gmail.com>
2023-09-08 23:37:33 +0900

    feat : JPAQueryFactory 빈 추가
    - 어플리케이션 설정 파일에 빈 등록 코드를 추가함
    - 설정파일이 분리될 경우 다른 파일로 옮길 수 있음

--- src/main/java/com/example/dm/ApplicationConfig.java
+++ src/main/java/com/example/dm/ApplicationConfig.java
@@ -1,7 +1,11 @@
 package com.example.dm;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -16,4 +20,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ServletComponentScan("com.example")
 @Configuration
 public class ApplicationConfig {
+
+    @PersistenceContext
+    public EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
 }
