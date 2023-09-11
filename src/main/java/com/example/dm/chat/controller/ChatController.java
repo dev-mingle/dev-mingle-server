5f7462fb381fac301ffac24ee580f9dcb3b7131d
minoolian <mw.lee@kakao.com>
2023-09-11 22:42:38 +0900

    feat: 채팅메시지 Endpoint 컨트롤러 추가
    - 채팅 참여 엔드포인트
    - 채팅 전송 엔드포인트

--- src/main/java/com/example/dm/chat/controller/ChatController.java
+++ src/main/java/com/example/dm/chat/controller/ChatController.java
@@ -1,21 +1,30 @@
 package com.example.dm.chat.controller;
 
-import com.example.dm.chat.entity.ChatRoom;
-import com.example.dm.chat.service.ChatRoomService;
+import com.example.dm.chat.dto.ChatDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final ChatRoomService chatRoomService;
+    private final SimpMessageSendingOperations template;
 
-    @PostMapping
-    public ChatRoom createRoom(String name) {
-        return chatRoomService.createRoom(name);
+    @MessageMapping("/chat/enter")
+    public void enterUser(@Payload ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
+
+        headerAccessor.getSessionAttributes().put("roomId", chatDto.getRoomId());
+
+        chatDto.setMessage(chatDto.getSender() + "님이 입장하셨습니다.");
+        template.convertAndSend("/sub/chat/room/" + chatDto.getRoomId(), chatDto);
+    }
+
+    @MessageMapping("/chat/message")
+    public void sendMessage(@Payload ChatDto chatDto) {
+        template.convertAndSend("/sub/chat/room/"+chatDto.getRoomId(), chatDto);
     }
 }
