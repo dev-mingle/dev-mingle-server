98090643523ba26cf9640b6491f9d66e9b4ed09f
minoolian <mw.lee@kakao.com>
2023-09-12 08:31:42 +0900

    feat: 채팅방 메시지 조회 쿼리 추가

--- src/main/java/com/example/dm/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/dm/chat/repository/ChatMessageRepository.java
@@ -1,7 +1,14 @@
 package com.example.dm.chat.repository;
 
+import com.example.dm.chat.dto.ChatMessageDto;
 import com.example.dm.chat.entity.ChatMessage;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    List<ChatMessageDto> findByRoomId(Long roomId, Sort sort);
+
 }
