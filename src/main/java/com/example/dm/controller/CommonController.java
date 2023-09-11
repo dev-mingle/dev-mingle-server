de69c1802efd3bc6a9c3a79f731f174407547b24
hcgo97 <hcgo_o@naver.com>
2023-09-12 00:05:47 +0900

    feat: S3 PreSigned URL 생성 비즈니스 로직 작성 및 컨트롤러 연결

--- src/main/java/com/example/dm/controller/CommonController.java
+++ src/main/java/com/example/dm/controller/CommonController.java
@@ -1,17 +1,31 @@
 package com.example.dm.controller;
 
 import com.example.dm.dto.ApiResponse;
+import com.example.dm.dto.commons.PreSignedUrlDto;
+import com.example.dm.service.S3Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("${api.path.default}/commons")
 public class CommonController extends BaseController {
+
+    private final S3Service s3Service;
+
+    /**
+     * PreSigned URL 요청
+     */
+    @PostMapping("/pre-signed-url")
+    public ResponseEntity<ApiResponse> getPreSignedUrl(@RequestBody PreSignedUrlDto.InputDto inputDto) {
+
+        List<PreSignedUrlDto.OutputDto> outputDtoList = s3Service.getPreSignedUrls(inputDto);
+        return responseBuilder(outputDtoList, HttpStatus.OK);
+    }
 
     /**
      * 테스트 API
