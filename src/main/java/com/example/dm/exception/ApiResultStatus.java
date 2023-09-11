79e279cfbadcac0f128c64e3e075228dd459c040
hcgo97 <hcgo_o@naver.com>
2023-09-12 04:54:47 +0900

    feat: 팔로우 취소관련 에러코드 추가

--- src/main/java/com/example/dm/exception/ApiResultStatus.java
+++ src/main/java/com/example/dm/exception/ApiResultStatus.java
@@ -34,7 +34,8 @@ public enum ApiResultStatus {
     NOT_MY_POST("E4031", "해당 게시물에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN),
     FOLLOW_ALREADY_EXIST("E4092", "이미 팔로우한 유저입니다.", HttpStatus.CONFLICT),
     CANNOT_FOLLOW_MYSELP("E4221", "자기 자신을 지정할 수 없습니다.", HttpStatus.UNPROCESSABLE_ENTITY),
-    INVALID_ORDER_TYPE("E4002", "잘못된 정렬 조건 입니다. [%s]", HttpStatus.BAD_REQUEST);
+    INVALID_ORDER_TYPE("E4002", "잘못된 정렬 조건 입니다. [%s]", HttpStatus.BAD_REQUEST),
+    FOLLOW_NOT_FOUND("E4043", "존재하지 않는 팔로우 정보입니다.", HttpStatus.NOT_FOUND);
 
     private final String code;
     private final String message;
