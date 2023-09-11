ef9529461ea8ba514e3738d9c9877d7e6a6df6f9
minoolian <mw.lee@kakao.com>
2023-09-12 08:44:21 +0900

    feat: 채팅방 참여 및 메시지 전송 핸들러 메서드 추가
    - 채팅방 참여시 참여인원 알림 메시지 전송 메서드
    - 채팅방 메시지 전송 핸들러 메서드

--- src/main/java/com/example/dm/chat/controller/ChatController.java
+++ src/main/java/com/example/dm/chat/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.dm.chat.controller;
 
 import com.example.dm.chat.dto.ChatDto;
+import com.example.dm.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,17 +15,27 @@ public class ChatController {
 
     private final SimpMessageSendingOperations template;
 
-    @MessageMapping("/chat/enter")
-    public void enterUser(@Payload ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
+    private final ChatMessageService chatMessageService;
 
+    private static final String SUBSCRIBE_URL = "/sub/v1/chats/";
+
+    // todo: pathvariable로 roomId 받는 경우
+    // todo: 각 room이 존재하는지 유효성, user가 채팅방에 속해있는지 유효
+    @MessageMapping("/v1/chats/join")
+    public void enterUser(@Payload ChatDto chatDto, SimpMessageHeaderAccessor headerAccessor) {
         headerAccessor.getSessionAttributes().put("roomId", chatDto.getRoomId());
 
-        chatDto.setMessage(chatDto.getSender() + "님이 입장하셨습니다.");
-        template.convertAndSend("/sub/chat/room/" + chatDto.getRoomId(), chatDto);
+        chatDto.setMessage(chatDto.getSender() + " 님이 입장하셨습니다.");
+        processMessage(chatDto);
     }
 
-    @MessageMapping("/chat/message")
+    @MessageMapping("/v1/chats/message")
     public void sendMessage(@Payload ChatDto chatDto) {
-        template.convertAndSend("/sub/chat/room/"+chatDto.getRoomId(), chatDto);
+        processMessage(chatDto);
+    }
+
+    private void processMessage(ChatDto chatDto) {
+        template.convertAndSend(SUBSCRIBE_URL + chatDto.getRoomId(), chatDto);
+        chatMessageService.saveMessage(chatDto);
     }
 }
