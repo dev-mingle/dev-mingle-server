d2eed7cb6429719e215a23d9f9a9c671eccd9b28
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-09-15 02:54:18 +0900

    create: jwt 토큰 설정
    - auth 인증관련 yml 파일 생성
    - application.yml에서 jwt 관련 사항 주석처리
    - TokenFilter 생성
    - TokenProvider 생성 (토큰 생성, 검증 등)

---
+++ src/main/java/com/example/dm/security/jwt/TokenFilter.java
@@ -0,0 +1,59 @@
+package com.example.dm.security.jwt;
+
+import com.example.dm.exception.ApiResultStatus;
+import com.example.dm.exception.AuthException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.util.Map;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class TokenFilter extends OncePerRequestFilter {
+  private final TokenProvider tokenProvider;
+  private final Key key;
+
+  @Value("${jwt.access.header}")
+  private String accessHeader;
+
+  @Value("${jwt.refresh.header}")
+  private String refreshHeader;
+
+  @Override
+  protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                  @NonNull HttpServletResponse response,
+                                  @NonNull FilterChain filterChain) {
+    String accessToken = resolveToken(request.getHeader(accessHeader));
+    String refreshToken = resolveToken(request.getHeader(refreshHeader));
+
+    if(accessToken==null || refreshToken==null){
+      throw new AuthException(ApiResultStatus.TOKEN_NOT_FOUND.getMessage());
+    }
+
+    tokenProvider.validateToken(accessToken);
+    if(tokenProvider.expiredToken(accessToken) && !tokenProvider.expiredToken(refreshToken)){
+      tokenProvider.validateToken(refreshToken);
+      Map<String,String> map = tokenProvider.rebuildToken(refreshToken);
+      accessToken = map.get("accessToken");
+      refreshToken = map.get("refreshToken");
+    }
+
+    tokenProvider.setAuthenticationTokens(response, accessToken, refreshToken);
+  }
+
+  private String resolveToken(String bearerToken) {
+    if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+      return bearerToken.substring(7);
+    }
+    return null;
+  }
+}
