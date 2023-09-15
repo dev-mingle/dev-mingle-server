13123c777ec43261f21d99f9f1fef05110e4727f
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-09-16 03:17:58 +0900

    modified: 코드정리
    - AuthController 불필요 주석 삭제
    - TokenFilter 네이밍 일치

--- src/main/java/com/example/dm/controller/AuthController.java
+++ src/main/java/com/example/dm/controller/AuthController.java
@@ -24,7 +24,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -136,10 +135,6 @@ public class AuthController extends BaseController {
       accessToken = tokenProvider.generateAccessToken(loginUser);
       refreshToken = tokenProvider.generateRefreshToken(loginUser);
     }
-
-//    Authentication authentication = tokenProvider.getAuthentication(accessToken);
-//    SecurityContextHolder.getContext().setAuthentication(authentication);
-
     tokenProvider.setAuthenticationTokens(response, accessToken, refreshToken);
   }
 
