dba5885bea0884b7e9e8a972ee063f9119568c36
minoolian <mw.lee@kakao.com>
2023-09-19 15:43:48 +0900

    feat: 채팅방과 유저 조인 테이블 추가
    다대다 매핑을 위한 조인테이블

---
+++ src/main/java/com/example/dm/chat/entity/ChatRoomUserProfiles.java
@@ -0,0 +1,28 @@
+package com.example.dm.chat.entity;
+
+import com.example.dm.entity.UserProfiles;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomUserProfiles {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_profiles_id")
+    private UserProfiles userProfiles;
+
+    @ManyToOne
+    @JoinColumn(name = "chat_room_id")
+    private ChatRoom chatRoom;
+}
