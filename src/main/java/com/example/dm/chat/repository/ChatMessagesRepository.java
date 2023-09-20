15d217eeb83267e59c93a598bc71cda851eb20b4
minoolian <mw.lee@kakao.com>
2023-09-21 07:22:12 +0900

    rename: Repository명 변경
    Entity 클래스명이 바뀜에 따른 변화

--- src/main/java/com/example/dm/chat/repository/ChatMessagesRepository.java
+++ src/main/java/com/example/dm/chat/repository/ChatMessagesRepository.java
@@ -1,14 +1,14 @@
 package com.example.dm.chat.repository;
 
-import com.example.dm.chat.dto.ChatMessageDto;
+import com.example.dm.chat.dto.ChatMessageGetDto;
 import com.example.dm.chat.entity.ChatMessages;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface ChatMessageRepository extends MongoRepository<ChatMessages, String> {
+public interface ChatMessagesRepository extends MongoRepository<ChatMessages, String> {
 
-    List<ChatMessageDto> findByRoomId(Long roomId, Sort sort);
+    List<ChatMessageGetDto> findByRoomId(Long roomId, Sort sort);
 
 }
