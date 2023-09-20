b1e5ac53f30844d3c6cab5a64218f4247c6709d1
minoolian <mw.lee@kakao.com>
2023-09-21 07:19:59 +0900

    feat: ChatRoomGetDto from 메서드 추가

--- src/main/java/com/example/dm/chat/dto/ChatRoomGetDto.java
+++ src/main/java/com/example/dm/chat/dto/ChatRoomGetDto.java
@@ -1,5 +1,6 @@
 package com.example.dm.chat.dto;
 
+import com.example.dm.chat.entity.ChatRooms;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,5 +13,13 @@ public class ChatRoomGetDto {
     private String name;
 
     private int userCount;
+
+    public static ChatRoomGetDto from(ChatRooms chatRooms) {
+        return ChatRoomGetDto.builder()
+                .roomId(chatRooms.getId())
+                .name(chatRooms.getName())
+                .userCount(chatRooms.getUserCount())
+                .build();
+    }
 
 }
