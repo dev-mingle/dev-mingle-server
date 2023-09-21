caa282bd066c5ef2fc2bacfe5494ba23869e7bbe
minoolian <mw.lee@kakao.com>
2023-09-21 09:24:52 +0900

    fix: Mongodb key 에러 해결
    Data Mongodb key 생성은 uuid(string)

--- src/main/java/com/example/dm/chat/entity/ChatMessages.java
+++ src/main/java/com/example/dm/chat/entity/ChatMessages.java
@@ -13,7 +13,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class ChatMessages extends BaseTimeEntity {
 
     @Id
-    private Long id;
+    private String id;
 
     private Long roomId;
 
