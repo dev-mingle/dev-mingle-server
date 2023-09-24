2e4c78e643aaf5c8bcf9f566f59826d77a9048ff
gwanuk <guk1502012@gmail.com>
2023-09-24 14:17:41 +0900

    chore : build test 추가
    - .env 파일에 추가된 환경변수 README.md에 설명 추가
    - h2 의존성 추가
    - test application.yml 추가
    - DevMingleApplicationTests -> DevMingleApplicationTest 이름 변경
    - start-server.sh 실행시 빌드 테스트에 필요한 환경변수 .env파일에서 읽어오도록 수정
    - 빌드 테스트 성공 확인

--- src/test/java/com/example/dm/DevMingleApplicationTest.java
+++ src/test/java/com/example/dm/DevMingleApplicationTest.java
@@ -3,11 +3,9 @@ package com.example.dm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest - 테스트 코드가 작성되지 않아서 일단 주석처리
-class DevMingleApplicationTests {
-
+@SpringBootTest
+class DevMingleApplicationTest {
     @Test
     void contextLoads() {
     }
-
 }
