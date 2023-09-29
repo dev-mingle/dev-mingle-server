e2c9d5fcacf839de0d97c1e91fb25bb8fca91302
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-09-29 18:46:20 +0900

    add TokenFilterTest 생성
    - 토큰 검증 필요여부에 따른 url 테스트
    - 불필요 import, 메서드 삭제

--- src/main/java/com/example/dm/dto/users/PasswordValidator.java
+++ src/main/java/com/example/dm/dto/users/PasswordValidator.java
@@ -1,30 +1,18 @@
 package com.example.dm.dto.users;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 class PasswordValidator implements ConstraintValidator<Password, String> {
-  private static final Logger logger = LoggerFactory.getLogger(PasswordValidator.class);
-
   @Value("${valid.password.pattern}")
   private String PASSWORD_PATTERN;
   @Value("${valid.password.message.empty}")
   private String PASSWORD_NULL_MESSAGE;
   @Value("${valid.password.message.err}")
   private String PASSWORD_ERR_MESSAGE;
-
-  @PostConstruct
-  public void postConstruct() {
-    logger.info("PASSWORD_PATTERN: {}", PASSWORD_PATTERN);
-    logger.info("PASSWORD_NULL_MESSAGE: {}", PASSWORD_NULL_MESSAGE);
-    logger.info("PASSWORD_ERR_MESSAGE: {}", PASSWORD_ERR_MESSAGE);
-  }
 
   @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
