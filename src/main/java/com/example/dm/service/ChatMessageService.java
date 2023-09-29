333215fa21f3e312e1cb0541164f7976f83018c5
minoolian <mw.lee@kakao.com>
2023-09-29 10:45:16 +0900

    feat: 메시지 조회 참여 유저 유효성 검증 추가

--- src/main/java/com/example/dm/service/ChatMessageService.java
+++ src/main/java/com/example/dm/service/ChatMessageService.java
@@ -3,7 +3,12 @@ package com.example.dm.service;
 import com.example.dm.dto.chats.ChatCreateDto;
 import com.example.dm.dto.chats.ChatMessageGetDto;
 import com.example.dm.entity.ChatMessages;
+import com.example.dm.entity.ChatRooms;
+import com.example.dm.entity.LoginUser;
+import com.example.dm.exception.ApiResultStatus;
+import com.example.dm.exception.BusinessException;
 import com.example.dm.repository.ChatMessagesRepository;
+import com.example.dm.repository.ChatRoomsRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -18,8 +23,14 @@ public class ChatMessageService {
 
     private final ChatMessagesRepository chatMessagesRepository;
 
+    private final ChatRoomsRepository chatRoomsRepository;
+
     // todo: room별 저장?
-    public List<ChatMessageGetDto> findAllMessageByRoomId(Long roomId) {
+    public List<ChatMessageGetDto> findAllMessageByRoomId(Long roomId, LoginUser user) {
+        ChatRooms chatRooms = verifyRoom(roomId);
+
+        if(!verifyJoinUser(user, chatRooms)) throw new BusinessException(ApiResultStatus.USER_NOT_EXIST_ROOM);
+
         return chatMessagesRepository.findByRoomId(roomId, Sort.by(Sort.Direction.ASC, "createdAt"));
     }
 
@@ -28,5 +39,13 @@ public class ChatMessageService {
 
         ChatMessages message = chatMessagesRepository.save(chatMessages);
         return ChatMessageGetDto.from(message);
+    }
+
+    private ChatRooms verifyRoom(Long roomId) {
+        return chatRoomsRepository.findById(roomId).orElseThrow(() -> new BusinessException(ApiResultStatus.ROOM_NOT_FOUND));
+    }
+
+    private boolean verifyJoinUser(LoginUser user, ChatRooms chatRooms) {
+        return chatRooms.getChatMembers().stream().anyMatch(members -> members.getUserProfiles().getId().equals(user.getId()));
     }
 }
