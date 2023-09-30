a6e587d29ff2e1dfce3b71d52ddbebda32953c13
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-10-01 01:28:21 +0900

    add AuthControllerTest 생성
    - 이메일, 닉네임 중복확인, 회원가입, 로그인 테스트
    - 테스트를 위한 SignupDto @Builder 추가

--- src/main/java/com/example/dm/dto/users/SignupDto.java
+++ src/main/java/com/example/dm/dto/users/SignupDto.java
@@ -3,6 +3,7 @@ package com.example.dm.dto.users;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@Builder
 @AllArgsConstructor
 public class SignupDto {
     @NotBlank
