a4c7774d1abdd09c2a116d925bc0dc7b19458b6b
gwanuk <guk1502012@gmail.com>
2023-10-01 18:55:44 +0900

    test : posts 테스트 추가
    - 테스트 sql파일 추가
    - controller 테스트 수정
    - service 테스트 추가
    - repository 테스트 추가

--- src/test/java/com/example/dm/controller/PostsControllerTest.java
+++ src/test/java/com/example/dm/controller/PostsControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.dm.controller;
 
+import com.example.dm.security.jwt.TokenFilter;
 import com.example.dm.service.PostsService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = PostsController.class,
-        excludeAutoConfiguration = SecurityAutoConfiguration.class)
+        excludeAutoConfiguration = SecurityAutoConfiguration.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ASSIGNABLE_TYPE,
+                classes = TokenFilter.class))
 class PostsControllerTest {
 
     @Autowired
@@ -33,26 +36,25 @@ class PostsControllerTest {
     private PostsService postsService;
 
     @Test
-    @DisplayName("게시글 목록 조회시 데이터가 없을 경우 빈 리스트 반환")
-    void findAll_empty() throws Exception {
+    @DisplayName("게시글 목록 조회 PostListDto, ApiResponse")
+    void findAll() throws Exception {
         // given
-        String search = "backend";
-        String conditions = "title,content";
-        String location = "cl,37.619774,127.060926";
-        String page = "0";
-        String size = "20";
-        String sort = "createdAt,desc";
-        PageRequest pageable = PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "createdAt"));
-        given(postsService.findAll(anyLong(), anyString(), any(), any(), any())).willReturn(Page.empty(pageable));
+        given(postsService.findAll(
+                        eq(1L),
+                        eq("backend"),
+                        eq(new String[]{"title", "content"}),
+                        eq(new double[]{37.619774, 127.060926}),
+                        eq(PageRequest.of(0, 20, Sort.by(Sort.Direction.DESC, "createdAt")))))
+                .willReturn(Page.empty());
 
-        // when
+        // expected
         mockMvc.perform(get("/api/v1/posts/{categoryId}", 1)
-                        .param("search", search)
-                        .param("conditions", conditions)
-                        .param("location", location)
-                        .param("page", page)
-                        .param("size", size)
-                        .param("sort", sort)
+                        .param("search", "backend")
+                        .param("conditions", "title,content")
+                        .param("location", "37.619774,127.060926")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "createdAt,desc")
                         .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.txid").isNotEmpty())
@@ -60,14 +62,79 @@ class PostsControllerTest {
                 .andExpect(jsonPath("$.message").value("정상적으로 처리되었습니다."))
                 .andExpect(jsonPath("$.data.content").isEmpty())
                 .andExpect(jsonPath("$.data.number").value("0"))
-                .andExpect(jsonPath("$.data.size").value("20"))
+                .andExpect(jsonPath("$.data.size").value("0"))
                 .andDo(print());
+    }
 
+    @Test
+    @DisplayName("validation check categoryId:must not be nul")
+    void findAll_category_id_null() throws Exception {
+        // expected
+        mockMvc.perform(get("/api/v1/posts/{categoryId}", " ")
+                        .param("search", "backend")
+                        .param("conditions", "title,content")
+                        .param("location", "37.619774,127.060926")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "createdAt,desc")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.txid").isNotEmpty())
+                .andExpect(jsonPath("$.status").value("422"))
+                .andDo(print());
+    }
 
-        //then
-        then(postsService).should().findAll(eq(1L), eq("backend"),
-                eq(new String[]{"title", "content"}) ,
-                eq(new String[]{"cl", "37.619774", "127.060926"}),
-                eq(pageable));
+    @Test
+    @DisplayName("validation check categoryId:Failed to convert")
+    void findAll_category_id_string() throws Exception {
+        // expected
+        mockMvc.perform(get("/api/v1/posts/{categoryId}", "aaa")
+                        .param("search", "backend")
+                        .param("conditions", "title,content")
+                        .param("location", "37.619774,127.060926")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "createdAt,desc")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.txid").isNotEmpty())
+                .andExpect(jsonPath("$.status").value("422"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("validation check location: size must be between 2 and 2")
+    void findAl_location_size() throws Exception {
+        // expected
+        mockMvc.perform(get("/api/v1/posts/{categoryId}", "1")
+                        .param("search", "backend")
+                        .param("conditions", "title,content")
+                        .param("location", "")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "createdAt,desc")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.txid").isNotEmpty())
+                .andExpect(jsonPath("$.status").value("422"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("validation check location:Failed to convert")
+    void findAl_location_string() throws Exception {
+        // expected
+        mockMvc.perform(get("/api/v1/posts/{categoryId}", "1")
+                        .param("search", "backend")
+                        .param("conditions", "title,content")
+                        .param("location", "37.619774,aaa")
+                        .param("page", "0")
+                        .param("size", "20")
+                        .param("sort", "createdAt,desc")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isUnprocessableEntity())
+                .andExpect(jsonPath("$.txid").isNotEmpty())
+                .andExpect(jsonPath("$.status").value("422"))
+                .andDo(print());
     }
 }
