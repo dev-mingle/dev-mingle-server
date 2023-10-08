5a251a19ccd0adf2c2b2caa27eab4ec540478e14
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-10-08 09:19:17 +0900

    add 사용자 관련 테스트코드
    - auth, user controller
    - SecurityConfigTest
    - Token 테스트

---
+++ src/test/java/com/example/dm/controller/AuthControllerTest.java
@@ -0,0 +1,142 @@
+package com.example.dm.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.example.dm.dto.users.LoginDto;
+import com.example.dm.dto.users.SignupDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("AuthController 테스트")
+class AuthControllerTest {
+  @Resource
+  MockMvc mockMvc;
+  @Resource
+  ObjectMapper objectMapper;
+
+  @Autowired
+  PasswordEncoder passwordEncoder;
+
+  @Value("${jwt.access.header}")
+  private String ACCESS_HEADER;
+
+  @Value("${jwt.refresh.header}")
+  private String REFRESH_HEADER;
+
+  // 테스트를 위한 공용 변수
+  String email = "devmingle11@gmail.com";
+  String nickname = "nickname";
+  String password = "Passord1234#";
+
+
+
+  @Test
+  @DisplayName("이메일 인증발급 확인")
+  void sendOtp() {
+    try {
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/users/otp")
+              .param("email", email)
+              .contentType(MediaType.APPLICATION_JSON)
+      );
+      resultActions
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.data").value(email))
+          .andDo(print());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @DisplayName("닉네임 중복체크 테스트")
+  void checkNickname(){
+    try {
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/users/nickname")
+              .param("nickname", nickname)
+              .contentType(MediaType.APPLICATION_JSON)
+      );
+      resultActions
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.data").value(nickname))
+          .andDo(print());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @Rollback(false)
+  @DisplayName("회원가입 테스트")
+  void signup() {
+    SignupDto dto = getSignupDto();
+    try {
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/users")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(dto))
+      );
+      resultActions
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.data.createdAt").isNotEmpty())
+          .andExpect(jsonPath("$.data.email").value(email))
+          .andExpect(jsonPath("$.data.userProfile.nickname").value(nickname))
+          .andExpect(header().exists(ACCESS_HEADER))
+          .andExpect(header().exists(REFRESH_HEADER))
+          .andDo(print());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  @DisplayName("로그인 테스트")
+  void login() {
+    LoginDto dto = new LoginDto(email, password);
+    try {
+      ResultActions resultActions = mockMvc.perform(
+          post("/api/v1/users/login")
+              .contentType(MediaType.APPLICATION_JSON)
+              .content(objectMapper.writeValueAsString(dto))
+      );
+      resultActions
+          .andExpect(status().isOk())
+          .andExpect(jsonPath("$.data").value(email))
+          .andExpect(header().exists(ACCESS_HEADER))
+          .andExpect(header().exists(REFRESH_HEADER))
+          .andDo(print());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  private SignupDto getSignupDto() {
+    return SignupDto.builder()
+        .email(email)
+        .password(password)
+        .nickname(nickname)
+        .city("city")
+        .state("state")
+        .street("street")
+        .build();
+  }
+}
---
+++ src/test/java/com/example/dm/util/MailSenderTest.java
@@ -0,0 +1,43 @@
+package com.example.dm.util;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.SendFailedException;
+import java.io.UnsupportedEncodingException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("메일 전송 테스트")
+class MailSenderTest {
+  @Autowired
+  MailSender mailSender;
+
+  private final String EMAIL = "devmingle11@gamil.com";
+
+  // fromEmail 설정
+  // application-auth -> mail.email
+
+  /* 실제 메일 전송됨으로 주의 */
+  @Test
+  @DisplayName("이메일 인증 메일링 테스트")
+  void sendEmailVerification() throws MessagingException, UnsupportedEncodingException {
+    mailSender.sendEmailVerification(EMAIL);
+  }
+
+  /* 실제 메일 전송됨으로 주의 */
+  @Test
+  @DisplayName("랜덤 비밀번호 발송 테스트")
+  void sendRandomPwd() throws MessagingException, UnsupportedEncodingException {
+    mailSender.sendRandomPwd(EMAIL,"random_password");
+  }
+
+  @Test
+  @DisplayName("이메일 형식이 올바르지 않은 경우")
+  void exceptionTest() {
+    assertThrows(SendFailedException.class, () -> mailSender.sendMail(EMAIL, "subject", "contents"));
+  }
+}
