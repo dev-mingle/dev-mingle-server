9065079427d29fa5fddd4b850369efc3ec19ccf8
gwanuk <guk1502012@gmail.com>
2023-10-24 02:42:40 +0900

    fix : UpdateRetryAspect 수정
    - 트랜잭션 어드바이스보다 높은 우선순위로 수정 @Order(1)
    - 예외 메시지 수

--- src/main/java/com/example/dm/aspect/UpdateRetryAspect.java
+++ src/main/java/com/example/dm/aspect/UpdateRetryAspect.java
@@ -5,9 +5,11 @@ import com.example.dm.exception.UpdateFailureException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Component;
 
+@Order(1)
 @Aspect
 @Component
 public class UpdateRetryAspect {
@@ -27,6 +29,6 @@ public class UpdateRetryAspect {
             }
         }
 
-        throw new UpdateFailureException("게시물 수정을 잠시후 다시 시도해 주세요.", exception);
+        throw new UpdateFailureException("잠시후 다시 시도해 주세요.", exception);
     }
 }
